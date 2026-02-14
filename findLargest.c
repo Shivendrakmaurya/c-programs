40d32aea4ddc29094a32c2391bcc4711c61af5cb
Shivendrak maurya <shivendrakmaurya@gmail.com>
2026-02-14 21:58:10 +0530

    Added comments to all C programs

--- findLargest.c
+++ findLargest.c
@@ -1,11 +1,17 @@
+
+// C program to find the largest of three numbers
+
 #include <stdio.h>
 
 int main()
 {
                int a, b, c;
 
+               // Input three numbers from user
                printf("Enter three numbers = ");
                scanf("%d%d%d", &a, &b, &c);
+
+               // Checking the largest number among a,b and c
                if (a >= b && a >= c)
                {
                               printf("Largest number is = %d", a);
