a54f7adbc94ad88d545b7ae3b42761d2409e6f72
Shivendrak maurya <shivendrakmaurya@gmail.com>
2026-03-20 00:55:28 +0530

    To print pattern by nested loop

---
+++ pattern1.c
@@ -0,0 +1,20 @@
+// C program to print the pattern
+
+//Output: 
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+
+#include <stdio.h>
+int main()
+{
+               for (int i = 1; i <= 4; i++)
+               {
+                              for (int j = 1; j <= i; j++)
+                              {
+                                             printf("%d ", j);
+                              }
+                              printf("\n");
+               }
+}
